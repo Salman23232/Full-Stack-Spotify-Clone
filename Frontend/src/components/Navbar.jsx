1fca95ecbb4f5e56e1d953cd99ef332f8d4a559a
Salman Ahmed <sscvocational4@gmail.com>
2024-12-19 10:38:29 +0600

    done building the frontend

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import { assets } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate()
+
   return (
     <>
       <div className="w-full flex justify-between items-center font-semibold">
         <div className="flex items-center gap-2">
-          <img
+          <img onClick={()=>navigate(-1)} 
             src={assets.arrow_left}
             className="w-8 bg-black p-2 rounded-2xl cursor-pointer"
             alt=""
           />
-          <img
+          <img onClick={()=>navigate(1)}
             src={assets.arrow_right}
             className="w-8 bg-black p-2 rounded-2xl cursor-pointer"
             alt=""
--- Frontend/src/components/SongItems.jsx
+++ Frontend/src/components/SongItems.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { useContext } from 'react'
+import { PlayerContext } from '../context/PlayerContext'
 
 const SongItems = ({name,image,desc,id}) => {
+
+    const {playwithId} = useContext(PlayerContext)
+
+
+
   return (
-    <div className='min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]'>
+    <div onClick={()=>playwithId(id)} className='min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]'>
       <img src={image} alt="" className="rounded" />
       <p className="font-bold mt-2 mb-1">{name}</p>
       <p className='text-slate-200 text-sm'></p>

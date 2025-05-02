bc5af0e441ab9ae58aafb368844a736abf6d4d1c
Salman Ahmed <sscvocational4@gmail.com>
2025-05-02 22:19:30 +0600

    update

--- Backend/server.js
+++ Backend/server.js
@@ -19,7 +19,7 @@ connectCloudinary();
 //middlewares
 app.use(express.json());
 app.use(cors({
-    origin:['https://musiclovers2.onrender.com', 'https://musify-lake-one.vercel.app']
+    origin:['https://musiclovers2.onrender.com', 'https://adminmusify.vercel.app']
 }
 ))
     
